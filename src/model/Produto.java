5beb2a6b3f7f7d93875fd13b746d89ec781ce31e
Petterson <shinokzn1227@gmail.com>
2025-04-11 22:45:27 -0300

    Estrutura da classe abstrata Produto

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,4 +1,32 @@
 package model;
 
-public class Produto {
+public abstract class Produto {
+    //Atributos
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    //Construtor
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    //Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
